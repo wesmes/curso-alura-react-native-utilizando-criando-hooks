5ce75605035222a3d90cef03f79a9801c86296cd
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-02-24 21:50:39 -0300

    Módulo 1 - Aula 5: Rodando o projeto

---
+++ orgsHooks/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App() {
+    
+    return <SafeAreaView><Text>Teste</Text></SafeAreaView>;
+
+}
+
+export default App;
