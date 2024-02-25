c05146b669bfa2b5be96b0332cec95971a7bee6d
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-02-24 22:25:55 -0300

    Módulo 2 - Aula 2: Componente do topo

--- orgsHooks/App.js
+++ orgsHooks/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+import Home from './src/telas/Home';
 
 function App() {
     
-    return <SafeAreaView><Text>Teste</Text></SafeAreaView>;
+    return <SafeAreaView>
+                <Home />
+            </SafeAreaView>;
 
 }
 
